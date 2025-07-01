fa0ed39f41a3c361933baba8b73d10007301742b
whoiscc <whoiscc.github@gmail.com>
2025-07-01 16:12:31 +0800

    misc: update sort with async key experiment

--- misc/exp_sort_with_async_key.py
+++ misc/exp_sort_with_async_key.py
@@ -1,9 +1,41 @@
-def working_key(n):
-    return -n
+from asyncio import run
+from random import randrange
+from time import time
 
-async def my_key(n):
-    return -n
 
-print(sorted([1, 2, 3], key=working_key))
-# TypeError: '<' not supported between instances of 'coroutine' and 'coroutine'
-[1, 2, 3].sort(key=my_key)
+def sync_key(n):
+    return n
+
+
+async def async_key(n):
+    return n
+
+
+print(sorted([1, 2, 3], key=sync_key))
+try:
+    [1, 2, 3].sort(key=async_key)
+except TypeError as err:
+    print(err)
+
+
+def run_async_key(n):
+    return run(async_key(n))
+
+
+print(sorted([1, 2, 3], key=run_async_key))
+
+
+xs = [randrange(1 << 20) for _ in range(1 << 16)]
+start = time()
+xs.sort()
+print("no key", time() - start)
+
+xs = [randrange(1 << 20) for _ in range(1 << 16)]
+start = time()
+xs.sort(key=sync_key)
+print("sync key", time() - start)
+
+xs = [randrange(1 << 20) for _ in range(1 << 16)]
+start = time()
+xs.sort(key=run_async_key)
+print("async key", time() - start)
